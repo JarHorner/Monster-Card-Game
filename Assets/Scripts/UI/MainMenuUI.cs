eca9b49e919360134b65b859056d89e2f9aafac6
Jarret Horner <JHorn384@mtroyal.ca>
2024-04-07 17:55:47 -0600

    Update MainMenuUI.cs

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
+    [SerializeField] private Button PlayButton;
+    [SerializeField] private Button QuitButton;
+
     // Start is called before the first frame update
     void Start()
+    {
+        PlayButton.onClick.AddListener(PlayOnClick);
+        QuitButton.onClick.AddListener(QuitOnClick);
+    }
+
+    private void PlayOnClick()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void QuitOnClick()
     {
-        
+        Application.Quit();
     }
+
 }
